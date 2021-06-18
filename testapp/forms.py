394762b69459b05bb7a2c3b6730fe43bce157e67
sivaram <sivaramk1411@gmail.com>
2021-06-18 17:59:45 +0530

    name field form validation in forms.py

--- testapp/forms.py
+++ testapp/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 class StudentForm(forms.Form):
-    name=forms.CharField(max_length=124)
+    name=forms.CharField()
     rollno=forms.IntegerField()
     gmail=forms.EmailField()
     feedback=forms.CharField(widget=forms.Textarea)
+    def clean_name(self):
+        inputname=self.cleaned_data['name']
+        if len(inputname)<4:
+            raise forms.ValidationError("the length of the name should be >=4")
+        else:
+            return inputname
